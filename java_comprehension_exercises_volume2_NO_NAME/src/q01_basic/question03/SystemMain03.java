0a71f114d21b81e985efbff5b69a1c76da81e572
emakane <emakane@C010423136.zipangu.cac.co.jp>
2026-04-28 16:09:54 +0900

    0428 q01_basic.question04まで完了
    理解できた点
    ・コンストラクタの仕組み、働き
    
    不明な点、調査中
    ・NonMemberクラスにおけるエラー原因（特にオーバーライドについて）

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question03/SystemMain03.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question03/SystemMain03.java
@@ -4,6 +4,12 @@ public class SystemMain03 {
 
 	public static void main(String[] args) {
 		//TODO ここから実装する
+		Member member = new Member();
+
+		Member memberM = new Member("Miura Manabu", 28, 2);
+
+		member.showMember();
+		memberM.showMember();
 
 	}
 
